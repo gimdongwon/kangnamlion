b57c8fb205b8d69a0272797e20562eb0c424cf24
gimdongwon <ehddnjs8989@gmail.com>
2022-07-14 14:18:59 +0900

    (feat) git second id로 token hardcording으로 git sh 수정

---
+++ git-pull.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib/openssl-1.1
+cd kangnamlion
+git pull https://kimdongwon99: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@github.com/gimdongwon/kangnamlion.git
