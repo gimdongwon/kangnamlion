580d2268e21c7b42142763c231b48a5c30f36392
gimdongwon <ehddnjs8989@gmail.com>
2022-07-18 01:44:08 +0900

    (feat) opensea kakaolink 변경 klay추가 및 원화 계산 추가 개발 중

--- Bots/opensea/index.js
+++ Bots/opensea/index.js
@@ -1,38 +1,44 @@
-const scriptName = 'opensea';
+const env = JSON.parse(FileStream.read('sdcard/msgbot/env.json'));
+const { KakaoLinkClient } = require('kakaolink');
+const Kakao = new KakaoLinkClient(env['KAKAO_CLIENT_KEY'], 'https://developers.kakao.com');
+Kakao.login(env['KAKAO_ID'], env['KAKAO_PASSWORD']); // 카카오 계정 아이디와 비밀번호
+
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
-  const koreanNameDict = {
-
-  };
   if (msg.startsWith('옾')) {
-    try{
+    try {
+      const dict_data = JSON.parse(FileStream.read('sdcard/msgbot/dict.json'));
       const temp = msg.split(' ');
-      let symbol = temp[1];
-      /*if (koreanNameDict[temp[1]]!==''){
-        symbol = koreanNameDict[temp[1]];
+      let target = temp[1];
+      if (Object.keys(dict_data).indexOf(target) > -1) {
+        target = dict_data[target];
       }
-      */
-      let result = opensea_func(symbol);
-      let output_text = ''
-      output_text += 'nft opensea 조회 \n';
-      output_text += '이름 : ' + symbol + '\n';
-      output_text +=
-        '바닥가 : ' + result.collection.stats['floor_price'].toFixed(2) + ' ' +result.collection.payment_tokens[0]['symbol'] + '\n';
-      // output_text += '이미지 url (카카오 링크로 그림 추가 예정): ' + result.collection['banner_image_url'] + '\n';
-      output_text += '하루 거래량 : ' + result.collection.stats['one_day_volume'].toFixed(2);
-      replier.reply(output_text);
-    } catch(error){
-     replier.reply('호출 실패￦n' + error)
-      }
-
+      const result = opensea_func(target);
+      const imgUrl = result.collection.banner_image_url;
+      const floorPrice = result.collection.stats['floor_price'] && result.collection.stats['floor_price'].toFixed(4);
+      const averagePrice =
+        result.collection.stats['average_price'] && result.collection.stats['average_price'].toFixed(4);
+      const oneDayVolume = result.collection.stats['one_day_volume'].toFixed(4);
+      const totalVolume = result.collection.stats['total_volume'].toFixed(4);
+      Kakao.sendLink(
+        room,
+        {
+          template_id: 79059,
+          template_args: {
+            title: target,
+            imgUrl: imgUrl,
+            floorPrice: floorPrice,
+            averagePrice: averagePrice,
+            oneDayVolume: oneDayVolume,
+            totalVolume: totalVolume,
+          },
+        },
+        'custom'
+      );
+      // replier.reply(output_text);
+    } catch (error) {
+      replier.reply('호출 실패￦n' + error);
+    }
   }
-  /* if (msg.startsWith('사전')){
-    const temp2 = msg.split(' ');
-    let engName = temp2[1], korName = temp2[2];
-    koreanNameDict[korName] = engName;
-    replier.reply('명령어 추가완료 ' + engName + ' '+korName);
-
-  }
-*/
 }
 
 function opensea_func(opensea_symbol) {
