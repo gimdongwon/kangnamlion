1c0ef7506dab1389992bc76ea0dfe676ae3a46bf
gimdongwon <ehddnjs8989@gmail.com>
2022-08-06 19:35:52 +0900

    (feat) 주식 구글, 네이버 에서 크롤링으로 변경(진행중)

--- Bots/invest/index.js
+++ Bots/invest/index.js
@@ -3,14 +3,10 @@ const { KakaoLinkClient } = require('kakaolink');
 const Kakao = new KakaoLinkClient(env['KAKAO_CLIENT_KEY'], 'https://developers.kakao.com');
 Kakao.login(env['KAKAO_ID'], env['KAKAO_PASSWORD']); // 카카오 계정 아이디와 비밀번호
 
-// const { KakaoLinkClient: KakaoLinkClient } = require('kakaolink');
-// const Kakao = new KakaoLinkClient('806c389b4ca349f0748523283881e63d', 'https://developers.kakao.com');
-// Kakao.login('dongwon@likelion.org', 'ehddnjs89!'); // 카카오 계정 아이디와 비밀번호
-
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
   const dict_data = JSON.parse(FileStream.read('sdcard/msgbot/dict.json'));
 
-  var str_split_Arr = msg.split(' ');
+  const str_split_Arr = msg.split(' ');
   let market = str_split_Arr[0],
     target = str_split_Arr[1];
   if (sender === '양용기') return;
@@ -19,59 +15,96 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
       if (Object.keys(dict_data).indexOf(target) > -1) {
         target = dict_data[target];
       }
-      var data = org.jsoup.Jsoup.connect('https://www.google.com/search?q=주식%20' + target.replace(/ /g, '%20')).get();
-      /*
-      var image_url = org.jsoup.Jsoup.connect(
-        'https://www.google.com/search?q=' +
-          target +
-          '&tbm=isch&source=iu&ictx=1&vet=1&fir=g5jEFZm-CqPVkM%252CQA-iVW8NtZ45yM%252C_&usg=AI4_-kRTrYTU4b2yOUtsz6SZrkIUD-VpiQ&sa=X&ved=2ahUKEwiG2J-bu5j4AhUZet4KHY7yDyQQ_B16BAgHEAI#imgrc=g5jEFZm-CqPVkM' +
-          target.replace(/ /g, '%20')
-      ).get();
-      */
-      //image_url = image_url.select('img[jsname=HiaYvf]').attr('src');
-      var image_url = data.select('div[class=gyEfO JlxBoc]').text();
-      // var image_url = data.select('div.JlxBoc > div.PZPZlf').attr('data-lpage');
-      data = data.select('g-card-section').get(0);
+      let data = org.jsoup.Jsoup.connect('https://www.google.com/search?q=주식%20' + target.replace(/ /g, '%20')).get();
+      const dataLength = data.select('g-card-section.N9cLBc');
+      // replier.reply('점검중');
+      // replier.reply(dataLength.length);
 
-      var title = data.select('span[class=aMEhee PZPZlF]').text();
-      var current_price = data.select('span[jsname=vWLAgc]').text();
-      var currency = data.select('span[jsname=T3Us2d]').text();
-      var profit = data.select('span[jsname=qRSVye]').text();
-      var percent = data.select('span[class=jBBUv]').text();
-      var prior_price = data.select('span[jsname=wurNO]').text();
-      var prior_percent = data.select('span[jsname=mHOGHd]').text();
-      // data = data.select('g-card-section').get(3);
-      var max_price = data.select('span[data-attrid=최고]').text();
-      var min_price = data.select('span[data-attrid=최저]').text();
-      var end_price;
-      if (prior_price) {
-        var temp = current_price;
-        current_price = prior_price;
-        end_price = temp;
+      // if (dataLength.length > 0) {
+
+      // const imageUrl = data.select('div[class=gyEfO JlxBoc]').text();
+      data = data.select('g-card-section').get(0);
+      const title = data.select('span[class=aMEhee PZPZlF]').text();
+
+      let currentPrice = data.select('div.wGt0Bc > div.PZPZlf > span > span > span[jsname=vWLAgc]').text();
+
+      const currency = data.select('span[jsname=T3Us2d]').text();
+
+      const profitPrice = data.select('span[class=WlRRw IsqQVc fw-price-dn] > span[jsname=qRSVye]').text();
+
+      const percent = data.select('span[class=jBBUv]').text();
+
+      const priorPrice = data.select('span[jsname=wurNO]').text();
+
+      const priorPercent = data.select('span[jsname=mHOGHd]').text();
+
+      data = org.jsoup.Jsoup.connect('https://www.google.com/search?q=주식%20' + target.replace(/ /g, '%20')).get();
+
+      const maxPrice = data.select('div[data-attrid="최고"]').text();
+      const minPrice = data.select('div[data-attrid="최저"]').text();
+
+      // 프리마켓 반영
+      let endPrice = currentPrice;
+      if (priorPrice) {
+        const temp = currentPrice;
+        currentPrice = priorPrice;
+        endPrice = temp;
       }
-      // tsuid17
       Kakao.sendLink(
         room,
         {
           template_id: 77842,
           template_args: {
             title: title,
-            current_price: current_price,
+            currentPrice: currentPrice,
             currency: currency,
-            profit: profit,
+            profitPrice: profitPrice,
             percent: percent,
-            prior: prior_price,
-            prior_percent: prior_percent,
-            max_price: max_price,
-            min_price: min_price,
-            image_url: image_url,
-            end_price: end_price,
+            priorPrice: priorPrice,
+            priorPercent: priorPercent,
+            maxPrice: maxPrice,
+            minPrice: minPrice,
+            endPrice: endPrice,
+            // imageUrl: imageUrl,
+            // dataLength: dataLength,
           },
         },
         'custom'
       );
-    } catch (e) {
-      replier.reply(e);
-    }
+
+      // } else {
+      //   replier.reply('네이버 ' + dataLength.text());
+      //   let newData = org.jsoup.Jsoup.connect(
+      //     'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=주식%20' +
+      //       target.replace(/ /g, '%20')
+      //   ).get();
+
+      //   const title = newData.select('span[class=stk_nm]').text();
+
+      //   const currentPrice = newData.select('span.spt_con > strong')[0].text();
+
+      //   const percent = newData.select('span.spt_con > span.n_ch > em')[0].text();
+
+      //   const priorPrice = newData.select('li.pcp > dl > dd').text();
+
+      //   const maxPrice = newData.select('li.hp > dl > dd').text();
+      //   const minPrice = newData.select('li.lp > dl > dd').text();
+      //   Kakao.sendLink(
+      //     room,
+      //     {
+      //       template_id: 77842,
+      //       template_args: {
+      //         title: title,
+      //         currentPrice: currentPrice,
+      //         percent: percent,
+      //         prior: priorPrice,
+      //         maxPrice: maxPrice,
+      //         minPrice: minPrice,
+      //       },
+      //     },
+      //     'custom'
+      //   );
+      // }
+    } catch (e) {}
   }
 }
