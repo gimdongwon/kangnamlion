78c3db6a4adff3e7ef69265c3c6bee54c77e25a8
seunggabi <seunggabi@naver.com>
2022-08-18 05:18:44 +0900

    (#4) refactor: search

--- Bots/search/index.js
+++ Bots/search/index.js
@@ -1,15 +1,18 @@
 const full = '\u200b'.repeat(1000);
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB) {
-  const data = org.jsoup.Jsoup.connect('http://rank.ezme.net/').get();
-
-  if (msg == '실검') {
-    const searchDatas = data.select('div.main-card > div.mdl-card__actions > h4 > a > b');
-    let result = `실시간 검색순위\n\n${full}`;
-    for (let i = 0; i < searchDatas.length; i++) {
-      result += `${i + 1} ${searchDatas[i].text()}\n`;
-    }
-    result += '\nezme 순위기록 검색어 사용';
-    replier.reply(result);
+  if (msg !== '실검') {
+    return;
   }
+
+  const data = org.jsoup.Jsoup.connect('http://rank.ezme.net/').get();
+  const search = data.select('div.main-card > div.mdl-card__actions > h4 > a > b');
+
+  let result = `실시간 검색순위\n\n${full}`;
+  for (let i = 0; i < search.length; i += 1) {
+    result += `${i + 1} ${search[i].text()}\n`;
+  }
+  result += '\nezme 순위기록 검색어 사용';
+
+  replier.reply(result);
 }
