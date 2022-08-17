9d8c9b01fbdc3a1a962d2dca3cf617a70718ed3d
seunggabi <seunggabi@naver.com>
2022-08-18 08:33:10 +0900

    (#4) refactor: opensea

--- Bots/lotto/index.js
+++ Bots/lotto/index.js
@@ -1,5 +1,8 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName) {
-  if (sender === '용키') return;
+  if (sender === '용키') {
+    return;
+  }
+
   if (msg === '로또') {
     const data = org.jsoup.Jsoup.connect(
       'https://search.naver.com/search.naver?ie=UTF-8&query=%EB%A1%9C%EB%98%90%EB%B2%88%ED%98%B8&sm=chr_hty',
@@ -8,7 +11,6 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
     date = date.split(' 이전 회차 다음 회차')[0];
     const numbers = data.select('div.winning_number').text();
     const plusNum = data.select('div.bonus_number').text();
-
     replier.reply(`${date} \n\n\n${numbers} +${plusNum}`);
   } else if (msg === '로또추천') {
     const lottos = [];
@@ -34,20 +36,20 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName)
 
     data = data.text().split(' ');
     data.push(plusNum);
-    // price logic
+
     let result = 0;
     for (let i = 0; i < 6; i++) {
       if (data.indexOf(numbers[i]) > -1) {
         result += 1;
       }
     }
+
     data.pop();
+
     let out_text = '';
     out_text += `내 로또번호 \n${numbers}\n`;
-
     out_text += `이번주 당첨번호 \n${data} +${plusNum}\n\n`;
     out_text += `번호가 ${result}개 일치합니다.(플러스번호 일수도 있습니다)`;
-
     replier.reply(out_text);
   }
 }
