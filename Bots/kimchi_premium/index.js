1722a9c3c7da1e46e8a90b04dceabf88f57e8167
seunggabi <seunggabi@naver.com>
2022-08-18 09:11:49 +0900

    (#4) refactor: kimchi

--- Bots/kimchi_premium/index.js
+++ Bots/kimchi_premium/index.js
@@ -1,109 +1,60 @@
-function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
-  if (msg === '김프') {
-    const coin = 'BTC';
-
-    // 업비트 btc 가격
-
-    var url = `http://crix-api-endpoint.upbit.com/v1/crix/candles/days/?code=CRIX.UPBIT.KRW-${coin}`;
-
-    const upbit = JSON.parse(org.jsoup.Jsoup.connect(url).ignoreContentType(true).get().text());
-
-    upbitPrice = upbit[0].tradePrice;
-
-    // 빗썸 btc 가격
-
-    var url = `https://api.bithumb.com/public/transaction_history/${coin}_KRW`;
-
-    const bith = JSON.parse(org.jsoup.Jsoup.connect(url).ignoreContentType(true).get().text());
-
-    bith_price = bith.data[0].price;
-
-    // 바이낸스 btc 가격
-
-    var url = 'https://api.binance.com/api/v1/ticker/allPrices';
-
-    const binance = JSON.parse(org.jsoup.Jsoup.connect(url).ignoreContentType(true).get().text());
-
-    const coin_name = `${coin}USDT`;
-
-    let i = 0;
-
-    while (1) {
-      if (i > 1372) {
-        break;
-      }
-
-      if (coin_name == binance[i].symbol) {
-        price = binance[i].price;
-
-        var price = Number(price);
-
-        if (price < 1000) {
-          price = price.toFixed(4);
-        }
-
-        break;
-      }
-
-      i++;
-    }
-
-    // 김프 계산
-
-    // 업비트 김프
-
-    gimp_u = upbitPrice - change2KRW(price);
-
-    gimp_u = (gimp_u / upbitPrice) * 100;
-
-    gimp_u = gimp_u.toFixed(2);
-
-    // 빗썸 김프
-
-    gimp_b = bith_price - change2KRW(price);
-
-    gimp_b = (gimp_b / upbitPrice) * 100;
-
-    gimp_b = gimp_b.toFixed(2);
-
-    // 출력
-
-    replier.reply(
-      `바이낸스\n[BTC : ${
-        divide(price)
-      }$ ]`
-        + `\n업비트 ${
-          gimp_u
-        }% : ${
-          divide(upbitPrice)
-        }원`
-        + `\n빗썸 ${
-          gimp_b
-        }% : ${
-          divide(bith_price)
-        }원`,
-    );
-  }
+function get(url) {
+  return JSON.parse(org.jsoup.Jsoup.connect(url).ignoreContentType(true).get().text());
 }
 
-// 환율
-
-function change2KRW(usd) {
-  const url = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD';
-
-  const exchange = JSON.parse(org.jsoup.Jsoup.connect(url).ignoreContentType(true).get().text());
-
-  basePrice = exchange[0].basePrice;
-
-  const result = usd * basePrice;
-
-  return result;
+function krw(usd) {
+  const exchange = get('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD');
+  return usd * exchange[0].basePrice;
 }
-
-// 천자리 표기
 
 function divide(num) {
   const parts = num.toString().split('.');
-
   return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',') + (parts[1] ? `.${parts[1]}` : '');
+}
+
+function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId) {
+  if (msg !== '김프') {
+    return;
+  }
+
+  const coin = 'BTC';
+  const name = `${coin}USDT`;
+
+  const upbit = get(`http://crix-api-endpoint.upbit.com/v1/crix/candles/days/?code=CRIX.UPBIT.KRW-${coin}`);
+  const upbitPrice = upbit[0].tradePrice;
+
+  const bith = get(`https://api.bithumb.com/public/transaction_history/${coin}_KRW`);
+  const bithPrice = bith.data[0].price;
+
+  const binance = get('https://api.binance.com/api/v1/ticker/allPrices');
+
+  let price = 0;
+  let i = 0;
+  while (i <= 1372) {
+    if (name === binance[i].symbol) {
+      price = Number(binance[i].price);
+
+      if (price < 1000) {
+        price = price.toFixed(4);
+      }
+
+      break;
+    }
+
+    i += 1;
+  }
+
+  let gimpU = upbitPrice - krw(price);
+  gimpU = (gimpU / upbitPrice) * 100;
+  gimpU = gimpU.toFixed(2);
+
+  let gimpB = bithPrice - krw(price);
+  gimpB = (gimpB / bithPrice) * 100;
+  gimpB = gimpB.toFixed(2);
+
+  replier.reply(
+    `바이낸스\n[BTC : ${divide(price)}$ ]`
+    + `\n업비트 ${gimpU}% : ${divide(upbitPrice)}원`
+    + `\n빗썸 ${gimpB}% : ${divide(bithPrice)}원`,
+  );
 }
