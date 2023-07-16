db1105ff430e2d6256f15b98a304fa8f016e1d9f
gimdongwon <ehddnjs8989@gmail.com>
2023-07-16 12:15:33 +0900

    (fix) #edit 에러 메시지 제거, 소소한 수정

--- Bots/macro/index.js
+++ Bots/macro/index.js
@@ -1,5 +1,5 @@
 function response(room, msg, sender, isGroupChat, replier, ImageDB) {
-  const targetArray = ['가즈', '가보자', '가주아'];
+  const targetArray = ['가즈아', '가보자', '가주아'];
   for (let i = 0; i < targetArray.length; i++) {
     if (msg.includes(targetArray[i])) {
       const answerArr = ['가즈아 😎', '가보자고 🦁', '가보자구 🔥'];
@@ -10,6 +10,11 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB) {
   }
   if (msg === '사자설명서') {
     replier.reply('https://taltube.tistory.com/41');
+    return;
+  }
+  if (msg === '도미') {
+    const domi = org.jsoup.Jsoup.connect('https://coinmarketcap.com/ko').get().select('a.cmc-link').get(4).text();
+    replier.reply(domi);
     return;
   }
 }
