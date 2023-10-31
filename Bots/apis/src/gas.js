8eb26c854b1b79244c10422a9b7950db485b128d
gimdongwon <ehddnjs8989@gmail.com>
2023-11-01 01:44:30 +0900

    (fix) 기위 변경 ui 재설정

--- Bots/apis/src/gas.js
+++ Bots/apis/src/gas.js
@@ -1,9 +1,9 @@
 function main(replier) {
   const url = 'https://etherscan.io/gastracker';
 
-  const lowPrice = org.jsoup.Jsoup.connect(url).get().select('div#divLowPrice > div.h4').text();
-  const avgPrice = org.jsoup.Jsoup.connect(url).get().select('div#divAvgPrice > div.h4').text();
-  const highPrice = org.jsoup.Jsoup.connect(url).get().select('div#divHighPrice > div.h4').text();
+  const lowPrice = org.jsoup.Jsoup.connect(url).get().select('div#divLowPrice > div.d-flex > div.h4').text();
+  const avgPrice = org.jsoup.Jsoup.connect(url).get().select('div#divAvgPrice > div.d-flex > div.h4').text();
+  const highPrice = org.jsoup.Jsoup.connect(url).get().select('div#divHighPrice > div.d-flex > div.h4').text();
   const baseGas = org.jsoup.Jsoup.connect(url).get().select('span#spanLowPriorityAndBase').text();
 
   let result = '⛽️ 이더리움 가스비 조회 ⛽️\n\n';
